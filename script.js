252c5d9686e9066cc5f8dcd1147c32e7d82483ff
amjad7541 <161500783+amjad7541@users.noreply.github.com>
2024-11-09 03:16:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function goToAbout() {
+    window.location.href = 'about.html';
+}
+
+function goToHome() {
+    window.location.href = 'index.html';
+}
+
+function goToWishes() {
+    window.location.href = 'wishes.html';
+}
